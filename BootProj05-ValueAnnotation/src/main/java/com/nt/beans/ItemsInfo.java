fc691cbc8368f1d60c16461386449a88ba7aa571
Roshan Acharya <Dev1@gmail.com>
2024-06-30 10:06:39 +0545

    app5

---
+++ BootProj05-ValueAnnotation/src/main/java/com/nt/beans/ItemsInfo.java
@@ -0,0 +1,23 @@
+package com.nt.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component("info")
+@PropertySource("com/nt/commons/inputs.properties")
+public class ItemsInfo {
+	@Value("${idly.price}")
+	public float idlyPrice;
+	@Value("${dosa.price}")
+	public float dosaPrice;
+	@Value("${poha.price}")
+	public float pohaPrice;
+	
+	@Override
+	public String toString() {
+		return "ItemsInfo [idlyPrice=" + idlyPrice + ", dosaPrice=" + dosaPrice + ", pohaPrice=" + pohaPrice + "]";
+	}
+	
+	
+}
